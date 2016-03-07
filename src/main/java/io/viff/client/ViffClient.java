a9d304d22c9a4924ddc621419bacb18a1138a42f
Yishan Wen <wittywys@gmaill.com>
2016-03-07 17:29:57 +0800

    [yishan] add take screenshot in viff client

--- src/main/java/io/viff/client/ViffClient.java
+++ src/main/java/io/viff/client/ViffClient.java
@@ -2,7 +2,13 @@ package io.viff.client;
 
 import io.viff.client.model.DiffResultWrapper;
 import io.viff.client.model.Resolution;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class ViffClient {
@@ -10,6 +16,7 @@ public class ViffClient {
     private Resolution resolution;
     private int projectId;
     private String tag;
+    private WebDriver driver;
 
     public ViffClient(Resolution resolution, int projectId, String tag) {
         this.resolution = resolution;
@@ -18,11 +25,16 @@ public class ViffClient {
     }
 
     public void setWebDriver(WebDriver driver) {
-
+        this.driver = driver;
     }
 
-    public void addScreenshot(){
+    public void addScreenshot() throws IOException {
+        if(driver == null) {
+            throw new RuntimeException("Need setting web driver before take screenshot!!");
+        }
 
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshot, new File("temp.png"));
     }
 
     public DiffResultWrapper viff(String targetTag, int targetBuildNumber){
