63fecef4a8393647683883f5567ed1f925f3f9d0
Yishan Wen <wittywys@gmaill.com>
2016-03-14 17:21:31 +0800

    [yishan] change rest call

--- src/main/java/io/viff/client/service/ViffRestService.java
+++ src/main/java/io/viff/client/service/ViffRestService.java
@@ -9,8 +9,12 @@ import java.util.Map;
 
 public interface ViffRestService {
     @Multipart
-    @POST("/upload")
-    Call<ResponseBody> uploadScreenshot(@PartMap Map<String, RequestBody> map);
+    @POST("/upload/{projectID}/{tag}")
+    Call<ResponseBody> uploadScreenshot(@Path("projectID") String projectID, @Path("tag") String tag, @PartMap Map<String, RequestBody> map);
+
+    @Multipart
+    @POST("/upload/{projectID}/{tag}/{buildNumber}")
+    Call<ResponseBody> uploadScreenshotWithBuildNumber(@Path("projectID") String projectID, @Path("tag") String tag, @Part("buildNumber") String buildNumber, @PartMap Map<String, RequestBody> map);
 
     @GET("/compare")
     Call<ResponseBody> viffDiffrent(@Query("from") String from, @Query("to") String to);
